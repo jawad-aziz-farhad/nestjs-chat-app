4639e634bdc8cfc3426ccea5ef940eebf968d257
jawad-aziz-farhad <jawad_aziz_farhad@yahoo.com>
2025-01-19 17:08:10 +0500

    Required modules imported in root app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, ChatModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(`${process.env.DB_PREFIX}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`),
+    AuthModule, 
+    ChatModule, 
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
